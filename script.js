513f9be95b688f441423860a0cd2f83c91c3896f
arslanquraishi <arslanquraishi999@gmail.com>
2024-11-20 23:37:41 +0500

    some change

--- script.js
+++ script.js
@@ -65,3 +65,16 @@ const typed = new Typed('.multiple-text', {
     backDelay: 1000,
     loop: true
 });
+
+
+
+
+// ======================== scroll reveel==============================
+ScrollReveal({ 
+    reset: true ,
+    distance:'80px',
+    duration:2000,
+    delay:20
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: top });
